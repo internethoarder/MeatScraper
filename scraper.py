b6d8ebbdce9bcf34fbb2150941db80b32851c40c
Matthew J. Klippert <thetechman454@gmail.com>
2024-01-18 14:01:23 -0800

    fixed how the meats print so the title goes with the price.

--- scraper.py
+++ scraper.py
@@ -12,7 +12,26 @@ soup = BeautifulSoup(page.content, 'html.parser')
 
 #title = soup.find("div",{"class":'product-title__name'}).get_text()
 #price = soup.findAll("span",{"class":'price'})
-for el in soup.find_all('span', attrs={'class': 'woocommerce-Price-amount amount'}):
-    print (el.get_text())
-
+#for el in soup.find_all('span', attrs={'class': 'woocommerce-Price-amount amount'}):
+ #   print (el.get_text())
+#for title in soup.find_all('h2', attrs={'class': 'woocommerce-loop-product__title'}):
+ #   print (title.get_text())
 #print(price)
+
+results = soup.find("ul",{"class":'products columns-3'})
+
+beef_elements = results.find_all("li", class_="product_tag-beef-boxes")
+
+for beef_elements in beef_elements:
+    price_element = beef_elements.find("span", class_="woocommerce-Price-amount amount")
+    title_element = beef_elements.find("h2", class_="woocommerce-loop-product__title")
+    print(title_element.text.strip())
+    print(price_element.text.strip())
+
+
+
+
+#beef_element = results.find_all("li", class_="none product type-product post-21079 status-publish first instock product_cat-best-sellers product_cat-boxed-beef product_cat-holiday product_tag-beef-boxes product_tag-holiday-gifts product_tag-brisket has-post-thumbnail shipping-taxable purchasable product-type-simple")
+
+#for beef_element in beef_element:
+  #  print(beef_element, end="\n"*2)
